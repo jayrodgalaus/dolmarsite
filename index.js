71a407e5be0bbf62ae447c2b30633dd31ba1f0c4
jayrodgalaus <globalInnuendo@gmail.com>
2025-04-10 21:13:24 +0800

    a

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Default route to serve the React app
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
